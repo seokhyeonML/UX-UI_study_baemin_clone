a5b2c95c3976ebbc164d71ab06807d593568f815
seokhyeonML <smreo123@mindslab.ai>
2020-11-15 23:10:46 +0900

    carousel commit

--- uxui_study/lib/view/carousel.dart
+++ uxui_study/lib/view/carousel.dart
@@ -1,64 +1,99 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Carousel extends StatefulWidget {
   @override
-  _CarouselState createState() => _CarouselState();
+  CarouselState createState() => CarouselState();
 }
 
-class _CarouselState extends State<Carousel> {
+class CarouselState extends State<Carousel> {
   CarouselController _controller = CarouselController();
-  int pagenum = 0;
-
+  int pageNum = 0;
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-        carouselController: _controller,
-        options: CarouselOptions(
-            viewportFraction: 1.0,
-            enlargeCenterPage: false,
-            height: 100,
-            autoPlay: true,
-            autoPlayInterval: Duration(seconds: 10),
-            autoPlayAnimationDuration: Duration(seconds: 1)),
-        items: imageSlider(context));
+    return Stack(
+        children : [
+          CarouselSlider(
+            carouselController: _controller,
+            options: CarouselOptions(
+              aspectRatio: 2,
+              viewportFraction: 1.0,
+              enlargeCenterPage: false,
+              autoPlay: true,
+              autoPlayInterval: Duration(seconds: 10),
+              autoPlayAnimationDuration: Duration(seconds: 1),
+              onPageChanged: (index, reason){
+                setState(() {
+                  pageNum = index;
+                });
+              }
+            ),
+            items: imageSlider(context)),
+          Positioned(
+            right: 74,
+            bottom: 11,
+            child: Container(
+              child: Row(
+                children: [
+                  Text((pageNum+1).toString(),
+                    style: GoogleFonts.notoSans(
+                      fontSize: 7.75,
+                      color: Color(0xFFFFFFFF),
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text(" / 3",
+                    style: GoogleFonts.notoSans(
+                        fontSize: 7.75,
+                        color: Color(0xFFFFFFFF)
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 8,
+            right: 0,
+            child: Container(
+              width: 66,
+              height: 22,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(180), bottomLeft: Radius.circular(180),),
+                color: Color(0xFFDBDBDB),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text("모두 보기", style: GoogleFonts.notoSans(
+                    fontSize: 8.25,
+                  ),),
+                  Icon(Icons.arrow_forward_ios_sharp, size: 16,),
+                ],
+              )
+            ),
+          )
+        ]
+    );
   }
+
 }
 
 List<Widget> imageSlider(BuildContext context) {
-  List<int> images = [1, 2, 3, 4, 5];
+  List<Image> images = [
+    Image.asset("assets/images/banner/main_banner_01@3x.jpg"),
+    Image.asset("assets/images/banner/main_banner_02@3x.jpg"),
+    Image.asset("assets/images/banner/main_banner_03@3x.jpg"),
+  ];
   List<Widget> imageSliders = images
-      .map((i) => Container(
-      child: Stack(children: [
+      .map((i) =>
         Container(
-          width: MediaQuery.of(context).size.width,
-          margin: EdgeInsets.symmetric(horizontal: 5.0),
-          decoration: BoxDecoration(color: Colors.amber),
-          child: new Image.network(
-              "https://cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/BMUUFU7EADBRPOWYGDA6E52RTQ.jpg",
-              fit: BoxFit.cover),
-        ),
-        Positioned(
-          right: 150,
-          bottom: 5,
-          child: Container(
-            child: Text(
-              i.toString() + "/" + images.asMap().length.toString(),
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-        ),
-        Positioned(
-          right: 5,
-          bottom: 5,
-          child: RaisedButton(
-              onPressed: () {
-                print("hi");
-              },
-              color: Colors.white,
-              child: Text("모두 보기")),
+          width: 360,
+          height: 180,
+          child: i,
         )
-      ])))
+      )
       .toList();
   return imageSliders;
 }
